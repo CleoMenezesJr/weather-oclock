07b2a742c00618f44c02df93f9759d6b5b5761f2
JasonLG1979 <jasonlevigray3@gmail.com>
2018-12-06 10:01:04 -0600

    Another crack at https://github.com/JasonLG1979/gnome-shell-extensions-weather-in-the-clock/issues/3

--- weatherintheclock@JasonLG1979.github.io/extension.js
+++ weatherintheclock@JasonLG1979.github.io/extension.js
@@ -15,22 +15,31 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * If this extension breaks your desktop you get to keep both pieces...
+ * If this extension breaks your desktop you get to keep all of the pieces...
  */
 
 "use strict";
 
 const Clutter = imports.gi.Clutter;
+const GLib = imports.gi.GLib;
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const PanelMenu = imports.ui.panelMenu;
 const Weather = imports.misc.weather;
 
-// Update the weather every 10 min.
-const UPDATE_TIMEOUT = 60 * 10;
+// Until we get valid weather info try every
+// 3 secs if a weather client is available and
+// has a valid location.
+const INT_UPDATE_INTERVAL = 3;
 
-let weatherItems = null;
+// Update the weather every 10 min in the long term.
+const LONG_TERM_UPDATE_INTERVAL = 60 * 10;
+
+// One sec.
+const UPDATE_THRESHOLD = 1000000;
+
+var weatherItems = null;
 
 function enable() {
     if (!weatherItems) {
@@ -44,8 +53,8 @@ function enable() {
 function disable() {
     if (weatherItems) {
         weatherItems.destroy();
+        weatherItems = null;
     }
-    weatherItems = null;
 }
 
 class WeatherItems {
@@ -60,36 +69,69 @@ class WeatherItems {
         });
         this.label.hide();
         this._weatherClient = new Weather.WeatherClient();
+        this._lastUpdate = 0;
+        this._weatherIntTimeoutId = null;
+        this._weatherLongTermTimeoutId = null;
+        this._weatherChangedId = this._weatherClient.connect('changed', () => {
+            let now = GLib.get_monotonic_time();
+            if ((now - this._lastUpdate) > UPDATE_THRESHOLD) {
+                this._update(now);
+            }
+        });
+        if (this._weatherClient.available && this._weatherClient.hasLocation) {
+            // We don't want to rapid fire timeouts if there is no weatherClient
+            // or it doesn't have a valid location.
+            this._weatherIntTimeoutId = Mainloop.timeout_add_seconds(INT_UPDATE_INTERVAL, () => {
+                this._weatherClient.update();
+                return true;
+            });
+        }
         if (this._weatherClient._useAutoLocation) {
             this._weatherClient._updateAutoLocation();
         }
-        this._weatherChangedId = this._weatherClient.connect('changed', this._onUpdate.bind(this));
-        this._weatherTimeoutId = Mainloop.timeout_add_seconds(UPDATE_TIMEOUT, () => {
-            this._weatherClient.update();
-            return true;
-        });
         this._weatherClient.update();
         
     }
 
     destroy() {
-        Mainloop.source_remove(this._weatherTimeoutId);
-        this._weatherClient.disconnect(this._weatherChangedId);
+        if (this._weatherIntTimeoutId) {
+            Mainloop.source_remove(this._weatherIntTimeoutId);
+        }
+        if (this._weatherLongTermTimeoutId) {
+            Mainloop.source_remove(this._weatherLongTermTimeoutId);
+        }
+        if (this._weatherChangedId) {
+            this._weatherClient.disconnect(this._weatherChangedId);
+        }
         this.label.destroy();
         this.icon.destroy();
         this._weatherClient = null;
-        this._weatherTimeoutId = null;
+        this._weatherIntTimeoutId = null;
+        this._weatherLongTermTimeoutId = null;
         this._weatherChangedId = null;
-        this._label = null;
-        this._icon = null;                
+        this.label = null;
+        this.icon = null;               
     }
 
-    _onUpdate() {
+    _update(now) {
         let iconName = null;
         let text = "";
         if (this._weatherClient.hasLocation && !this._weatherClient.loading) {
             let info = this._weatherClient.info;
             if (info.is_valid()) {
+                this._lastUpdate = now;
+                if (this._weatherIntTimeoutId) {
+                    // Once we have valid weather info remove the int timeout...
+                    Mainloop.source_remove(this._weatherIntTimeoutId);
+                    this._weatherIntTimeoutId = null;
+                }
+                if (!this._weatherLongTermTimeoutId) {
+                    // And start the long term timeout.
+                    this._weatherLongTermTimeoutId = Mainloop.timeout_add_seconds(LONG_TERM_UPDATE_INTERVAL, () => {
+                        this._weatherClient.update();
+                        return true;
+                    });
+                }
                 iconName = info.get_symbolic_icon_name();
                 text = info.get_temp_summary();
             }
