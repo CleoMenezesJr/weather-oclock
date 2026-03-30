84b21dbaccc4f95efa858dd01fc9f69a9066a377
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-03-30 17:50:14 -0300

    refactor: embed weather widget as sibling instead of replacing clock
    No longer wraps _clockDisplay in a custom container. The weather widget
    is inserted directly into the existing clock-display-box alongside the
    clock, leaving the clock widget untouched through enable/disable cycles.

--- weatheroclock@CleoMenezesJr.github.io/extension.js
+++ weatheroclock@CleoMenezesJr.github.io/extension.js
@@ -31,8 +31,7 @@ export default class WeatherOClock extends Extension {
   constructor(metadata) {
     super(metadata);
 
-    this._topBox = null;
-    this._originalClockDisplay = null;
+    this._clockDisplay = null;
     this._panelWeather = null;
     this._positionChangeListener = null;
     this._settings = null;
@@ -44,15 +43,8 @@ export default class WeatherOClock extends Extension {
     const networkIcon = network ? network._primaryIndicator : null;
     const weather = new Weather.WeatherClient();
 
-    this._originalClockDisplay = dateMenu._clockDisplay;
-    this._panelWeather = new PanelWeather(weather, networkIcon);
-
-    this._topBox = new St.BoxLayout({ style_class: "clock" });
-
-    this._originalClockDisplay.remove_style_class_name("clock");
-    this._originalClockDisplay
-      .get_parent()
-      .replace_child(this._originalClockDisplay, this._topBox);
+    this._clockDisplay = dateMenu._clockDisplay;
+    this._panelWeather = new PanelWeather(weather, networkIcon, this._clockDisplay);
 
     this._settings = this.getSettings();
     this._positionChangeListener = this._settings.connect(
@@ -69,47 +61,35 @@ export default class WeatherOClock extends Extension {
     }
     this._settings = null;
 
-    const clockDisplay = this._originalClockDisplay;
-    clockDisplay.remove_style_class_name("label-right-margin");
-    clockDisplay.remove_style_class_name("label-left-margin");
-    clockDisplay.add_style_class_name("clock");
-    clockDisplay.remove_all_transitions();
-    clockDisplay.translation_x = 0;
-
-    if (clockDisplay.get_parent() === this._topBox)
-      this._topBox.remove_child(clockDisplay);
-
     if (this._panelWeather) {
       this._panelWeather.destroy();
       this._panelWeather = null;
     }
 
-    this._topBox.get_parent()?.replace_child(this._topBox, clockDisplay);
-    this._topBox.destroy();
-    this._topBox = null;
-    this._originalClockDisplay = null;
+    if (this._clockDisplay) {
+      this._clockDisplay.remove_all_transitions();
+      this._clockDisplay.translation_x = 0;
+      this._clockDisplay = null;
+    }
   }
 
   _addWidget() {
-    const clockDisplay = this._originalClockDisplay;
+    const clockDisplay = this._clockDisplay;
+    const parent = clockDisplay.get_parent();
 
-    if (clockDisplay.get_parent() === this._topBox)
-      this._topBox.remove_child(clockDisplay);
-    if (this._panelWeather.get_parent() === this._topBox)
-      this._topBox.remove_child(this._panelWeather);
+    if (this._panelWeather.get_parent())
+      this._panelWeather.get_parent().remove_child(this._panelWeather);
 
-    clockDisplay.remove_style_class_name("label-right-margin");
-    clockDisplay.remove_style_class_name("label-left-margin");
+    this._panelWeather.remove_style_class_name("weather-before-clock");
+    this._panelWeather.remove_style_class_name("weather-after-clock");
 
     const isWeatherAfterClock = this._settings.get_boolean("weather-after-clock");
     if (isWeatherAfterClock) {
-      this._topBox.add_child(clockDisplay);
-      this._topBox.add_child(this._panelWeather);
-      clockDisplay.add_style_class_name("label-right-margin");
+      parent.insert_child_above(this._panelWeather, clockDisplay);
+      this._panelWeather.add_style_class_name("weather-after-clock");
     } else {
-      this._topBox.add_child(this._panelWeather);
-      this._topBox.add_child(clockDisplay);
-      clockDisplay.add_style_class_name("label-left-margin");
+      parent.insert_child_below(this._panelWeather, clockDisplay);
+      this._panelWeather.add_style_class_name("weather-before-clock");
     }
   }
 }
@@ -119,7 +99,7 @@ const PanelWeather = GObject.registerClass(
     GTypeName: "PanelWeather",
   },
   class PanelWeather extends St.BoxLayout {
-    _init(weather, networkIcon) {
+    _init(weather, networkIcon, clockDisplay) {
       super._init({
         visible: false,
         y_align: Clutter.ActorAlign.CENTER,
@@ -127,6 +107,7 @@ const PanelWeather = GObject.registerClass(
 
       this._weather = weather;
       this._networkIcon = networkIcon;
+      this._clockDisplay = clockDisplay;
       this._signals = [];
       this._weatherUpdateTimeout = null;
       this._retryTimeout = null;
@@ -185,6 +166,7 @@ const PanelWeather = GObject.registerClass(
       this._signals = null;
       this._weather = null;
       this._networkIcon = null;
+      this._clockDisplay = null;
       super.destroy();
     }
 
@@ -200,19 +182,18 @@ const PanelWeather = GObject.registerClass(
         const [, toWidth] = this.get_preferred_width(-1);
         const delta = toWidth - fromWidth;
 
-        if (parent && Math.abs(delta) > 2) {
+        const clockDisplay = this._clockDisplay;
+        if (parent && clockDisplay && Math.abs(delta) > 2) {
           const children = parent.get_children();
           const myIndex = children.indexOf(this);
-          children.forEach((child, i) => {
-            if (child === this) return;
-            const sign = myIndex < i ? -1 : 1;
-            child.remove_all_transitions();
-            child.translation_x = sign * delta / 2;
-            child.ease({
-              translation_x: 0,
-              duration: 500,
-              mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-            });
+          const clockIndex = children.indexOf(clockDisplay);
+          const sign = myIndex < clockIndex ? -1 : 1;
+          clockDisplay.remove_all_transitions();
+          clockDisplay.translation_x = sign * delta / 2;
+          clockDisplay.ease({
+            translation_x: 0,
+            duration: 500,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
           });
         }
 
