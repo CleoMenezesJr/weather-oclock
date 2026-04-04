e64bab541c29ac04fcdd943ce0f70a93172c920a
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-04-04 10:31:44 -0300

    chore: cleanup headers and remove third-party mentions

--- weatheroclock@CleoMenezesJr.github.io/prefs.js
+++ weatheroclock@CleoMenezesJr.github.io/prefs.js
@@ -1,20 +1,10 @@
-/****************************************************************************
- ** Weather O'Clock extension for GNOME Shell 45+
- ** Copyright (C) 2024  Cleo Menezes Jr.
- **
- ** This program is free software: you can redistribute it and/or modify
- ** it under the terms of the GNU General Public License as published by
- ** the Free Software Foundation, either version 3 of the License, or
- ** (at your option) any later version.
- **
- ** This program is distributed in the hope that it will be useful,
- ** but WITHOUT ANY WARRANTY; without even the implied warranty of
- ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- ** GNU General Public License for more details.
- **
- ** You should have received a copy of the GNU General Public License
- ** along with this program.  If not, see <https://www.gnu.org/licenses/>.
- ****************************************************************************/
+/*
+ * Weather O'Clock extension for GNOME Shell 45+
+ * Copyright 2022-2026 Cleo Menezes Jr.
+ *
+ * This software is released under the GNU General Public License v3 or later.
+ * See <http://www.gnu.org/licenses/> for details.
+ */
 "use strict";
 
 import Gtk from "gi://Gtk";
